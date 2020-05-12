743f49b8d8db01e3f3a692eb63394d5dc71191f3
Thao <nguyenvanthaobg1995@gmail.com>
2020-05-12 19:05:31 +0700

    fix as requirement

--- app/assets/javascripts/validate_email.js
+++ app/assets/javascripts/validate_email.js
@@ -2,13 +2,13 @@ $(window).on("turbolinks:load", function () {
   $(document).on("input","#user_email", function () {
     email_regex = /^([a-zA-Z0-9_\.])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
     $('.user_email').append('<div id="errors-message"></div>')
-    error_element = document.getElementById("errors-message")
-    if (!email_regex.test(this.value)) {
-      error_element.style.color = "red"
-      error_element.innerHTML = "invalid email";
-    }else{
-      error_element.style.color = "green"
-      error_element.innerHTML = "email is valid";
+    color_message = "red"
+    message = "invalid email"
+    if (email_regex.test(this.value)) {
+      color_message = "green"
+      message = "email is valid"
     }
+    $("#errors-message").css("color",color_message)
+    $("#errors-message").html(message)
   } )
 })
