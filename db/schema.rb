41695237d6f4ce4599feda7e8efef467a80265f2
Thao <nguyenvanthaobg1995@gmail.com>
2020-06-18 11:44:29 +0700

    fix conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_16_160420) do
+ActiveRecord::Schema.define(version: 2020_06_17_092100) do
 
   create_table "ckeditor_assets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "data_file_name", null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_06_16_160420) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["type"], name: "index_ckeditor_assets_on_type"
+  end
 
   create_table "course_types", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "course_type", limit: 50, null: false
