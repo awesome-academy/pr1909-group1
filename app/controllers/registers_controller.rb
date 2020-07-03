12d6f686925c8faadace858f256933c76c51a2c4
ngoctoandhv95 <ngoctoandhv95@gmail.com>
2020-07-03 09:18:35 +0700

    fix as requirement

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -2,13 +2,11 @@ class RegistersController < ApplicationController
   before_action :login_before_register
 
   def create
-    @course = Course.find params[:course_id]
     @register = Register.create(course_id: params[:course_id], user_id: current_user.id)
+    @course = Course.find params[:course_id]
+    @registers = @course.registers.order('created_at DESC')
     respond_to do |format|
       format.js
-      @course = Course.find params[:course_id]
-      @course_registers = @course.total_registers_count
-      @registers = @course.registers.order('created_at DESC')
     end
   end
 
