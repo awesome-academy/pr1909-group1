207a8965b1ae8c83d9cb8068f9f416985dfb5616
Thao <nguyenvanthaobg1995@gmail.com>
2020-07-28 22:56:26 +0700

    fix errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -79,14 +79,14 @@ end
 
 number_users = user_ids.length
 
-# number_users.times do |n|
-#   10.times do |m|
-#     Like.create!(
-#       user_id: user_ids[n],
-#       course_id: course_ids[m]
-#   )
-#   end
-# end
+number_users.times do |n|
+  10.times do |m|
+    Like.create!(
+      user_id: user_ids[n],
+      course_id: course_ids[m]
+  )
+  end
+end
 
 number_users.times do |n|
   sample = course_ids.sample(16)
